3f9d5ebd99eb8ba8bfc7db79cc1f617383ded4f2
msimon42 <msimon@uab.edu>
2020-03-19 22:13:25 -0600

    Add method to append list

--- lib/linked_list.py
+++ lib/linked_list.py
@@ -1,3 +1,14 @@
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def append(self, val):
+        if self.head is None:
+            self.head = val
+        else:
+            last = self.head
+            while last.next:
+                last = last.next
+            last.next = val
+
+                
