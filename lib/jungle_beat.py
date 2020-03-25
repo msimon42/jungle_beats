5182142c9e18697c37857509095bc0b34ad42faf
msimon42 <msimon@uab.edu>
2020-03-24 23:58:49 -0600

    Add method to add items

--- lib/jungle_beat.py
+++ lib/jungle_beat.py
@@ -1,3 +1,11 @@
 class JungleBeat:
     def __init__(self):
         self.list = LinkedList()
+
+    def append(self, data):
+        data_list = data.split(' ')
+
+        for item in data_list:
+            self.list.append(item)
+
+        return data    
